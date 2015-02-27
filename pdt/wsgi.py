718f556acba65410e7224c48101442257eb2bd30
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-02-27 19:28:28 +0100

    deployment

--- pdt/wsgi.py
+++ pdt/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 import os
 import sys
-sys.path = [os.path.dirname(__file__)]
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pdt.settings")
 
