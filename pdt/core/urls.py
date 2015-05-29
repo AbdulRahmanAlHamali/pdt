a21beac9fe743d7914a4e1c8f6ff1fde34d4857d
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-05-29 15:20:10 +0200

    change release notes url

--- pdt/core/urls.py
+++ pdt/core/urls.py
@@ -2,8 +2,8 @@
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'release-notes-overview$', view='pdt.core.admin.release_notes_overview',
+    url(r'release-notes/?$', view='pdt.core.admin.release_notes_overview',
         name='release-notes-overview'),
-    url(r'release-notes/(?P<release_number>[0-9]+)$', view='pdt.core.admin.release_notes',
+    url(r'release-notes/(?P<release_number>[0-9]+)/?$', view='pdt.core.admin.release_notes',
         name='release-notes'),
 ]
